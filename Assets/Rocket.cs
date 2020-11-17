c5fba5d82d73a6a1182f4ff8905ca1811cd61e5c
Felipe Marcolin <felipe.marcolin@hotmail.com>
2020-11-17 14:23:26 -0300

    rocket audio
    first audio source for the rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidbody;
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,7 +25,14 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidbody.AddRelativeForce(Vector3.up);
+            _rigidbody.AddRelativeForce(Vector3.up);
+
+            if(!_audioSource.isPlaying)
+                _audioSource.Play();
+        }
+        else
+        {
+            _audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
